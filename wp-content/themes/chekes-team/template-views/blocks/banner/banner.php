5302c852798bca8f99e53608f40b7e05d1830186
vaske454 <vasilijetomovic98@gmail.com>
2022-05-15 18:01:34 +0200

    Banner in progress.

--- wp-content/themes/chekes-team/template-views/blocks/banner/banner.php
+++ wp-content/themes/chekes-team/template-views/blocks/banner/banner.php
@@ -11,22 +11,61 @@
 $query_var = get_query_var( 'content-blocks', [] );
 
 // set and escape template view values
-$icon = $query_var['fws_svg_icon'] ?? '';
-$title = esc_textarea( $query_var['title'] ) ?? '';
-$subtitle = esc_textarea( $query_var['subtitle'] ) ?? '';
-$button = $query_var['button'] ? $query_var['button'] : [];
-$desktop_image = $query_var['desktop_image'] ?? [];
-$tablet_image = $query_var['tablet_image'] ?? [];
-$mobile_image = $query_var['mobile_image'] ?? [];
+//$icon = $query_var['fws_svg_icon'] ?? '';
+$title = $query_var['title'] ?? '';
+$description = $query_var['description'] ?? '';
+
+//shop link
+$shopLink = $query_var['shop_link'] ?? [];
+$shopLinkTitle = $shopLink['title'] ?? '';
+$shopLinkUrl = $shopLink['url'] ?? '';
+$shopLinkTarget = $shopLink['target'] ?? '';
+
+//call us link
+$callUsLink = $query_var['call_us_link'] ?? [];
+$callUsLinkTitle = $callUsLink['title'] ?? '';
+$callUsLinkUrl = $callUsLink['url'] ?? '';
+$callUsLinkTarget = $callUsLink['target'] ?? '';
+
+//banner image
+$bannerImage = $query_var['banner_image'] ?? [];
+$bannerImageAlt = $bannerImage['alt'] ?? '';
 ?>
 
 <div class="banner">
-	<?php fws()->render()->templateView($query_var, 'background-image', 'parts'); ?>
+	<?php get_template_part( 'template-views/parts/background-image/background-image' ); ?>
 
-	<div class="banner__caption">
-		<?php echo fws()->render()->inlineSVG( $icon, 'banner__caption-icon', true ); ?>
-		<h1 class="banner__caption-title js-scroll-link" data-scroll-to="slider"><?php echo $title; ?></h1>
-		<p class="banner__caption-text"><?php echo $subtitle; ?></p>
-		<?php echo fws()->acf()->linkField( $button, 'banner__btn btn' ); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="banner__content">
+					<?php if ($title): ?>
+					<h1 class="page-title gsap-page-title"><?= esc_html($title); ?></h1>
+					<?php endif; if ($description): ?>
+					<div class="entry-content">
+						<p><?= nl2br($description); ?></p>
+					</div>
+					<?php endif; if ($shopLinkTitle || $callUsLinkTitle): ?>
+					<div class="banner__buttons">
+						<?php if ($shopLinkTitle): ?>
+						<a class="btn banner__btn gasp-bottom" href="<?= esc_url($shopLinkUrl); ?>" target="<?= ($shopLinkTarget) ? '_blank' : '_self' ?>"><?= esc_html($shopLinkTitle); ?></a>
+						<?php endif; if ($callUsLinkTitle): ?>
+						<span class="banner__text gasp-top">or</span>
+						<span class="banner__link-holder gasp-bottom">Call Us: <a class="banner__link" href="<?= esc_url($callUsLinkUrl); ?>" target="<?= ($callUsLinkTarget) ? '_blank' : '_self' ?>"><?= esc_html($callUsLinkTitle); ?></a></span>
+						<?php endif; ?>
+					</div>
+					<?php endif; ?>
+				</div>
+			</div>
+			<?php if ($bannerImage): ?>
+			<div class="col-lg-6">
+				<div class="banner__image gasp-bottom">
+					<img src="<?php echo fws()->resizer()->newImageSize($bannerImage, 430, 808, false); ?>" alt="<?php echo esc_attr($bannerImageAlt); ?>">
+				</div>
+			</div>
+			<?php endif; ?>
+		</div>
 	</div>
+
 </div><!-- .banner -->
+
--- wp-content/themes/chekes-team/template-views/parts/background-image/background-image.php
+++ wp-content/themes/chekes-team/template-views/parts/background-image/background-image.php
@@ -11,24 +11,16 @@
 $query_var = get_query_var( 'content-parts', [] );
 
 // set and escape template view values
-$desktop_image = $query_var['desktop_image'] ?? [];
-$tablet_image = $query_var['tablet_image'] ?? [];
-$mobile_image = $query_var['mobile_image'] ?? [];
-$loader_image = fws()->resizer()->newImageSize($desktop_image['url'], 20, 7);
+//$desktop_image = $query_var['desktop_image'] ?? [];
+//$tablet_image = $query_var['tablet_image'] ?? [];
+//$mobile_image = $query_var['mobile_image'] ?? [];
+//$loader_image = fws()->resizer()->newImageSize($desktop_image['url'], 20, 7);
 ?>
 
-<?php if ( $desktop_image ) : ?>
 <picture class="background-image">
-	<source media="(min-width: 1200px)" srcset="<?php echo $loader_image ?>" data-srcset="<?php echo $desktop_image['url']; ?>">
+	<source media="(min-width: 1200px)" srcset="<?php echo fws()->images()->assetsSrc( 'banner-01.png', true ); ?>">
+	<source media="(min-width: 640px)" srcset="<?php echo fws()->images()->assetsSrc( 'banner-01.png', true ); ?>">
+	<source media="(min-width: 320px)" srcset="<?php echo fws()->images()->assetsSrc( 'banner-01.png', true ); ?>">
+	<img class="cover-img" src="<?php echo fws()->images()->assetsSrc( 'banner-01.png', true ); ?>" alt="">
+</picture>
 
-	<?php if ( $tablet_image ) : ?>
-		<source media="(min-width: 640px)" srcset="<?php echo $loader_image ?>" data-srcset="<?php echo $tablet_image['sizes']['large']; ?>">
-	<?php endif; ?>
-
-	<?php if ( $mobile_image ) : ?>
-		<source media="(min-width: 320px)" srcset="<?php echo $loader_image ?>" data-srcset="<?php echo $mobile_image['sizes']['medium']; ?>">
-	<?php endif; ?>
-
-	<img class="cover-img lazy" src="<?php echo $loader_image ?>" data-src="<?php echo $desktop_image['url']; ?>" alt="">
-</picture><!-- .background-image -->
-<?php endif; ?>
